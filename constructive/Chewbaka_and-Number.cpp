92c76a9a35494a4fefb0093e80b4f60c40f8f131
Shubham Sahu <shubham30sahu101@gmail.com>
2022-08-19 18:47:05 +0530

    2

--- constructive/Chewbaka_and-Number.cpp
+++ constructive/Chewbaka_and-Number.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 int main()
 {
+    long long int n, k;
+    cin >> n;
+    string s = "";
+    while (n > 0)
+    {
+        k = n % 10;
+        n = n / 10;
+        if (k >= 5)
+        {
+            if (k == 9 and n != 0)
+                k = 9 - k;
+            else if (k != 9 and n >= 0)
+                k = 9 - k;
+        }
+        s = to_string(k) + s;
+    }
+    cout << s << endl;
     return 0;
 }
