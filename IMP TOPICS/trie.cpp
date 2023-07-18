acdcfdbf942ae071a0110e2e5e9b690853aaf04d
SHUBHAM_SAHU <shubham30sahu101@gmail.com>
2023-07-18 20:33:10 +0530

    change

--- IMP TOPICS/trie.cpp
+++ IMP TOPICS/trie.cpp
@@ -1,84 +1,68 @@
-#include <bits/stdc++.h>
-using namespace std;
-struct Node
-{
-    Node* links[26];
-    bool flag = false;
-    bool containsKey(char ch)
-    {
-        return links[ch - 'a'] != nullptr;
-    }
-    void put(char ch,Node *node){
-        links[ch - 'a'] = node;
-    }
-    Node* get(char ch)
-    {
-        return links[ch - 'a'];
-    }
-    bool setEnd(){
-        flag = true;
-    }
-    bool isEnd()
-    {
-        return flag;
-    }
+struct Node {
+	Node *links[26];
+	bool flag = false;
+	//checks if the reference trie is present or not
+	bool containKey(char ch) {
+		return (links[ch - 'a'] != NULL);
+	}
+	//creating reference trie
+	void put(char ch, Node *node) {
+		links[ch - 'a'] = node;
+	}
+	//to get the next node for traversal
+	Node *get(char ch) {
+		return links[ch - 'a'];
+	}
+	//setting flag to true at the end of the word
+	void setEnd() {
+		flag = true;
+	}
+	//checking if the word is completed or not
+	bool isEnd() {
+		return flag;
+	}
 };
-class trie
-{
+class Trie {
 private:
-    Node *root;
-
+	Node* root;
 public:
-    trie()
-    {
-    }
-    // O(word.size())
-    void insert(string word)
-    {
-        Node *node = root;
-        for (int i = 0; i < word.length(); i++)
-        {
-            if(!node->containsKey(word[i]))
-            {
-                node->put(word[i],new Node());
-            }
-            // move to new reference trie
-            node = node->get(word[i]);
-        }
-        node->setEnd();
-    }
+	Trie() {
+		//creating new obejct
+		root = new Node();
+	}
 
+	void insert(string word) {
+		//initializing dummy node pointing to root initially
+		Node *node = root;
+		for (int i = 0; i < word.size(); i++) {
+			if (!node->containKey(word[i])) {
+				node->put(word[i], new Node());
+			}
+			//moves to reference trie
+			node = node->get(word[i]);
+		}
+		node->setEnd();
+	}
 
-    bool search(string word)
-    {
-        Node* node = root;
-        for(int i = 0;i<word.length();i++)
-        {
-            if(!node->containsKey(word[i]))
-            {
-                return false;
-            }
-            node = node->get(word[i]);
-        }
-        return node->isEnd();
-    }
+	bool search(string word) {
+		Node *node = root;
+		for (int i = 0; i < word.size(); i++) {
+			if (!node->containKey(word[i])) {
+				return false;
+			}
+			node = node->get(word[i]);
+		}
+		return node->isEnd();
+	}
 
-
-    bool startsWith(string prefix)
-    {
-        Node* node = root;
-        for(int i = 0;i<prefix.length();i++)
-        {
-            if(!node->containsKey(prefix[i]))
-            {
-                return false;
-            }
-            node = node->get(prefix[i]);
-        }
-        return true;
-    }
+	bool startsWith(string prefix) {
+		Node* node = root;
+		for (int i = 0; i < prefix.size(); i++) {
+			if (!node->containKey(prefix[i])) {
+				return false;
+			}
+			node = node->get(prefix[i]);
+		}
+		return true;
+	}
 };
-int main()
-{
-    return 0;
-}
