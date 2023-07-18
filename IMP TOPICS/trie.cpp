e2f943bac26053b0b1e490490e9e301cd9e352fe
SHUBHAM_SAHU <shubham30sahu101@gmail.com>
2023-07-18 13:25:14 +0530

    trie

---
+++ IMP TOPICS/trie.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    Node* links[26];
+    bool flag = false;
+    bool containsKey(char ch)
+    {
+        return links[ch - 'a'] != nullptr;
+    }
+    void put(char ch,Node *node){
+        links[ch - 'a'] = node;
+    }
+    Node* get(char ch)
+    {
+        return links[ch - 'a'];
+    }
+    bool setEnd(){
+        flag = true;
+    }
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+class trie
+{
+private:
+    Node *root;
+
+public:
+    trie()
+    {
+    }
+    // O(word.size())
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if(!node->containsKey(word[i]))
+            {
+                node->put(word[i],new Node());
+            }
+            // move to new reference trie
+            node->get(word[i]);
+        }
+        node->setEnd();
+    }
+
+
+    bool search(string word)
+    {
+        Node* node = root;
+        for(int i = 0;i<word.length();i++)
+        {
+            if(!node->containsKey(word[i]))
+            {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+
+
+    bool startsWith(string prefix)
+    {
+        Node* node = root;
+        for(int i = 0;i<prefix.length();i++)
+        {
+            if(!node->containsKey(prefix[i]))
+            {
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+};
+int main()
+{
+    return 0;
+}
